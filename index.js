43f37ff1e16e9dc0ac485cde387b6cfc3f2e4809
Joanna <astronautnemo@gmail.com>
2022-11-16 12:10:49 +0300

    Finished lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Follow along with the examples here
+
+function add(x, y){
+    return x+y;
+}
+console.log(add(1, 3));
+
+// function sayHelloTo(greeting, firstName){
+//     console.log(`${greeting}, ${firstName}!`);
+// }
+
+// console.log("Goodbye", "Julio");
+
+// function sayHelloTo(firstName){
+//     console.log(`Hello, ${firstName}!`);
+// }
+
+// sayHelloTo("Isabel");
+// sayHelloTo("Jane");
+// sayHelloTo("R2-D2");
+
+// console.log(firstName);
+
+// function sayHelloToIsabel(){
+//     console.log("Hello, Isabel!");
+// }
+
+// function sayHelloToSofia(){
+//     console.log("Hello, Sofia!");
+// }
+
+// function sayHelloToBrendan(){
+//     console.log("Hello, Brendan!");
+// }
+
+// sayHelloToIsabel();
+// sayHelloToSofia();
+// sayHelloToBrendan();
+
+
+// function sayHello() {
+//     console.log("Hello!");
+// }
+// sayHello();
